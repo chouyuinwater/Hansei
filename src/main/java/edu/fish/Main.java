69e4a30ff416d1c593dc53a2530b4a9c7a5fb8b5
jiangbing <jiangbing17@jd.com>
2018-08-08 10:11:00 +0800

    温故知新

--- src/main/java/edu/fish/Main.java
+++ src/main/java/edu/fish/Main.java
@@ -1,8 +1,14 @@
 package edu.fish;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List list = Collections.synchronizedList(new ArrayList());
+
     }
 }
