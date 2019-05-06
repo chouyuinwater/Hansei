cbaf0ff4ab1edc51edf20488a89192cb9ba5c3b6
jiangbing <jiangbing17@jd.com>
2019-05-06 18:54:07 +0800

    concurrent

--- src/main/java/hephaestus/MainTest.java
+++ src/main/java/hephaestus/MainTest.java
@@ -1,7 +1,20 @@
 package hephaestus;
 
+import java.util.regex.Pattern;
+
 public class MainTest {
     public static void main(String[] args) {
+
+        String value = "0.2";
+//        String parrten = "^[1-9][0-9]{0,7}(.[0-9]{1,3})?$";
+        String parrten = "^(0|[1-9][0-9]{0,7})(.[0-9]{1,3})?$";
+//        String parrten = "^?:(0|[1-9][0-9]{1,7})(\\.[0-9]{1,3})?$";
+//        String parrten = "^?:(0|[1-9][0-9]{1,7})(\\.[0-9]{1,3})?$";
+//        String parrten = "^?:(0|[1-9][0-9]{1,7})(\\.[0-9]{1,3})?$";
+        //"^(0|[1-9][0-9]{0,7})(.[0-9]{1,3})?$";
+
+        boolean result = Pattern.matches(parrten, value);
+        System.out.println(result);
 
     }
 }
